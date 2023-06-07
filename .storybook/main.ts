b64d91f115227c5e6c7d9e6531966956a8d0c842
doruk <doruksahin98@gmail.com>
2023-06-08 01:30:44 +0300

    allows to use modules just as you do normally

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,34 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type {StorybookConfig} from '@storybook/react-webpack5';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+const path = require('path');
+
+const baseUrl = './'; // Update this to match your tsconfig.json baseUrl
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
+  },
+  webpackFinal: async (config, {configType}) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+        extensions: config.resolve.extensions,
+        baseUrl,
+      }),
+    ];
+
+    return config;
   },
 };
 export default config;
